2645c995572dbf83a2ff829c8d295215425cf24e
Eyob Mekonnen <emekonnen35@gmail.com>
2024-11-02 21:36:52 +0300

    finalize the app

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -9,8 +9,14 @@ import {
 } from "react-native";
 import listingData from "@/data/destinations.json";
 import { ListingType } from "@/types/listingType";
-import { Feather, FontAwesome5, Ionicons } from "@expo/vector-icons";
+import {
+  Feather,
+  FontAwesome,
+  FontAwesome5,
+  Ionicons,
+} from "@expo/vector-icons";
 import Colors from "@/constants/Colors";
+import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
 const { width } = Dimensions.get("window");
 const IMG_HEIGHT = 300;
 const ListingDetails = () => {
@@ -69,6 +75,7 @@ const ListingDetails = () => {
         }}
       />
       <View style={styles.container}>
+        <ScrollView contentContainerStyle={{paddingBottom:150}}>
         <Image source={{ uri: listing.image }} style={styles.image} />
         <View style={styles.contentWrapper}>
           <Text style={styles.listingName}>{listing.name}</Text>
@@ -80,8 +87,58 @@ const ListingDetails = () => {
             />
             <Text style={styles.listingLocationTxt}>{listing.location}</Text>
           </View>
+          <View style={styles.highlightWrapper}>
+            <View style={{ flexDirection: "row" }}>
+              <View style={styles.highlightIcon}>
+                <Ionicons name="time" size={18} color={Colors.primaryColor} />
+              </View>
+              <View>
+                <Text style={styles.highlightTxt}>Duration</Text>
+                <Text style={styles.highlightTxtVal}>
+                  {listing.duration} Days
+                </Text>
+              </View>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <View style={styles.highlightIcon}>
+                <FontAwesome
+                  name="users"
+                  size={18}
+                  color={Colors.primaryColor}
+                />
+              </View>
+              <View>
+                <Text style={styles.highlightTxt}>Person</Text>
+                <Text style={styles.highlightTxtVal}>{listing.duration}</Text>
+              </View>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <View style={styles.highlightIcon}>
+                <Ionicons name="star" size={18} color={Colors.primaryColor} />
+              </View>
+              <View>
+                <Text style={styles.highlightTxt}>Rating</Text>
+                <Text style={styles.highlightTxtVal}>
+                  {listing.rating} Days
+                </Text>
+              </View>
+            </View>
+          </View>
+          <Text style={styles.listingDetails}>{listing.description}</Text>
         </View>
-        {/* <Text>Listing Detail {id}</Text> */}
+        </ScrollView>
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={[styles.footerBtn, styles.footerBookBtn]}
+        >
+          <Text style={styles.footerBtnTxt}>Book Now</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}} style={styles.footerBtn}>
+          <Text style={styles.footerBtnTxt}>${listing.price}</Text>
+        </TouchableOpacity>
+      
       </View>
     </>
   );
@@ -115,5 +172,58 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginLeft: 5,
     color: Colors.black,
+  },
+  highlightWrapper: {
+    flexDirection: "row",
+    marginVertical: 20,
+    justifyContent: "space-between",
+  },
+  highlightIcon: {
+    backgroundColor: "#F4F4F4",
+    paddingHorizontal: 8,
+    paddingVertical: 5,
+    borderRadius: 8,
+    marginRight: 5,
+    alignItems: "center",
+  },
+  highlightTxt: {
+    fontSize: 12,
+    color: "#999",
+  },
+  highlightTxtVal: {
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  listingDetails: {
+    fontSize: 16,
+    lineHeight: 25,
+    color: Colors.black,
+    letterSpacing: 0.5,
+  },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    padding: 20,
+    paddingBottom: 30,
+    width: width,
+    flexDirection: "row",
+  },
+  footerBtn: {
+    flex: 1,
+    backgroundColor: Colors.black,
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  footerBtnTxt: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: "600",
+    textTransform: "uppercase",
+  },
+  footerBookBtn: {
+    flex: 2,
+    backgroundColor: Colors.primaryColor,
+    marginRight: 20,
   },
 });
